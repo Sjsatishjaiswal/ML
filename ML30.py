ce873daff702470ef89a397b6c6b43d15ed991ed
Satish kumar Jaiswal <45370567+Sjsatishjaiswal@users.noreply.github.com>
2020-08-18 00:19:34 +0530

    L 2

---
+++ ML30.py
@@ -0,0 +1,78 @@
+we are with the sybllabus for ML
+
+
+Machine learning term was coined by sir Arthur Samuel of the 
+standford university in 1959.
+
+
+According to Dr. Samuel,
+
+
+"Machine Learning is the technique to train machines \
+    with data without explicit programming ."
+    
+    Dr. Samuel's"definition is a little vague."
+    The engineering oriented definition of ML was provided by 
+    Tom Michell of the standford university in the year 1999.
+    
+    
+    
+    ETPTPE
+    
+    "A machine is said to heve Learn from experience E over \
+        some task T with a performance measure P if  on  task T \
+            the Performance P increases with exprience E"
+            
+            
+   Task: Robot walking from point A to B 
+   Exprience:0
+   Performance :No of obstacles
+
+ At first training step
+
+Task:Robot walking from point A to B 
+   Exprience:1
+   Performance :Poor
+
+After training for a while 
+
+
+Task: Robot walking from point A to B 
+   Exprience:100
+   Performance :Good
+   
+   this can be termed as ML
+   
+   
+   
+   Classification of ML systems
+   
+   1.Whether or not it needs human superviseion
+        a)Supervised Learning
+        b)Unsupervised Learning
+        c)Semisupervised Learning
+        d) Reinforcement Learning
+    
+ 2.Wether or not it can incrementally Learn on the fly
+     a)Online based Learning
+    b) Batch Learning
+ 3. Whether it generalizes on known set of points or rather creates a model
+    
+   A) Model based Learning
+   B)instance based Learning
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
+   
+   
+   
