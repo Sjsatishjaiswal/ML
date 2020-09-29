9cbde642d5cca817e231e3e2eaabdc03ee787b2c
Satish kumar Jaiswal <45370567+Sjsatishjaiswal@users.noreply.github.com>
2020-09-29 22:59:28 +0530

    L 22

--- ml20.py
+++ ml20.py
@@ -1,48 +1,45 @@
+
+    
+                Dimensionality Reduction
+                
+In supervised learning, we mostly deal with tabular datasets which
+are represented as 2d collection of rows and columns.
+
+rows = m
+columns = n
+
+In a dataset, we can increase both rows and columns but only one
+of them is problematic. If we increase rows, it means we are
+getting more data which is a good thing. But if we increase
+columns or features then we are increasing the complexity of
+our model. 
+
+This phenomenon is known as curse of dimensionality.
+
+To solve the problem of curse of dimensionality we will use a
+powerful dimensionality reduction algorithm known as PCA.
+
+            PCA (Principal Component Analysis)
+            
+Principal : Main or Most Important
+Component : Features or Columns
+Analysis : Aap jaante he hai
+
+PCA uses a matrix factorization technique known as singular
+value decomposition (SVD) to divide the feature matrix into
+the dot product of three matrices. One of which gives the 
+principal features. The advantage of PCA is that it attempts to
+preserve maximum variance in the data.
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-col_names = ['age', 'workclass', 'fnlwgt', 'education',
-             'education-num', 'marital-status', 'occupation',
-             'relationship', 'race', 'gender', 'capital-gain',
-             'capital-loss', 'hours-per-week', 'native-country',
-             'salary']
+from sklearn.datasets import load_iris
+dataset = load_iris()
 
-
-dataset = pd.read_csv('adult_salary.csv', names = col_names,
-                      na_values = ' ?')
-
-dataset.isnull().sum()
-
-dataset_int = dataset.iloc[:, [0, 2, 4, 10, 11, 12]]
-dataset_str = dataset.iloc[:, [1, 3, 5, 6, 7, 8, 9, 13]]
-
-from sklearn.impute import SimpleImputer
-sim = SimpleImputer(strategy = 'most_frequent') 
-dataset_str = sim.fit_transform(dataset_str)
-
-dataset_str = pd.DataFrame(dataset_str)
-dataset_str.isnull().sum()
-
-dataset_str = pd.get_dummies(dataset_str)
-
-from sklearn.preprocessing import StandardScaler
-scaler = StandardScaler()
-dataset_int = scaler.fit_transform(dataset_int)
-
-dataset_int = pd.DataFrame(dataset_int)
-
-dataset_final = pd.concat([dataset_int, dataset_str], axis = 1)
-
-dataset_final.isnull().sum()
-
-X = dataset_final.values
-y = dataset.iloc[:, -1].values
-
-from sklearn.preprocessing import LabelEncoder
-lab = LabelEncoder()
-y = lab.fit_transform(y)
-lab.classes_
+X = dataset.data
+y = dataset.target
 
 from sklearn.linear_model import LogisticRegression
 log_reg = LogisticRegression()
@@ -50,15 +47,38 @@ log_reg.fit(X, y)
 
 log_reg.score(X, y)
 
-y_pred = log_reg.predict(X)
+from sklearn.decomposition import PCA
+pca = PCA(n_components = 3)
 
-from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y, y_pred)
+X_3 = pca.fit_transform(X)
+X_3
 
-from sklearn.metrics import precision_score, recall_score, f1_score
-precision_score(y, y_pred)
-recall_score(y, y_pred)
-f1_score(y, y_pred)
+pca1 = PCA(n_components = 2)
+
+X_2 = pca1.fit_transform(X)
+X_2
+
+pca2 = PCA(n_components = 1)
+
+X_1 = pca2.fit_transform(X)
+X_1
+
+log_reg.fit(X_3, y)
+log_reg.score(X_3, y)
+
+log_reg.fit(X_2, y)
+log_reg.score(X_2, y)
+
+log_reg.fit(X_1, y)
+log_reg.score(X_1, y)
+
+
+
+
+
+
+
+
 
 
 
